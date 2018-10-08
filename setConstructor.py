ce2e4f4c6cbf82c1a58607ab6a373b03d898b267
GiorgosMandi <giwrgosmandi@gmail.com>
2018-10-08 22:31:25 +0300

    generating a pseudo random id

--- setConstructor.py
+++ setConstructor.py
@@ -1,6 +1,12 @@
 import pandas as pd
 from wiki_parser import wiki_scraper
 
+import random
+import time
+random.seed = time.time()
+seed = random.randint(1000, 9999)
+
+# If the files exist they are loaded, else they are generated through wiki_scraper
 try:
     rc = pd.read_csv("Kapodistria_scheme/Regions_Counties.csv", sep='\t')
     cm = pd.read_csv("Kapodistria_scheme/Counties_Municipalities.csv", sep='\t')
@@ -8,6 +14,7 @@ try:
 except FileNotFoundError:
     rc, cm = wiki_scraper()
 
+# reads the entities from the dataframes
 regions = list(rc.columns)
 counties = []
 municipalities = []
@@ -17,9 +24,10 @@ for c in counties:
     municipalities += list(cm[c].dropna())
 municipalities = list(set(municipalities))
 
+# generates csv
 total_entities = regions + counties + municipalities
-count = list(range(len(total_entities)))
-df_total_entities = pd.DataFrame({'id': pd.Series(count), 'Administrative Unit': pd.Series(total_entities)})
+ids = [i+seed for i in range(len(total_entities))]
+df_total_entities = pd.DataFrame({'id': pd.Series(ids), 'Administrative Unit': pd.Series(total_entities)})
 df_total_entities.to_csv("Kapodistria_scheme/Kapodistria_AU.csv", sep='\t', index=False)
 
 print("Regions:\t\t", len(regions))
