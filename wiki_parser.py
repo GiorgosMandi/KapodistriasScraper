0e03922c4ccdc82d0da286392ad4ee8fc298a804
GiorgosMandi <giwrgosmandi@gmail.com>
2018-10-10 16:22:01 +0300

    changes in the pathes

--- wiki_parser.py
+++ wiki_parser.py
@@ -7,12 +7,12 @@ from bs4 import BeautifulSoup
 
 def wiki_scraper():
     try:
-        with open("Kapodistria_scheme/Wiki_Object", 'rb') as wo:
+        with open("datasets/Kapodistria_scheme/Wiki_Object", 'rb') as wo:
             page = pickle.load(wo)
     except FileNotFoundError:
         page = requests.get("https://el.wikipedia.org/wiki/%CE%94%CE%B9%CE%BF%CE%B9%CE%BA%CE%B7%CF%84%CE%B9%CE%BA%CE%AE_%CE%B4%CE%B9%CE%B1%CE%AF%CF%81%CE%B5%CF%83%CE%B7_%CF%84%CE%B7%CF%82_%CE%95%CE%BB%CE%BB%CE%AC%CE%B4%CE%B1%CF%82_1997")
         if page.status_code == 200:
-            with open("Kapodistria_scheme/Wiki_Object", 'wb') as wo:
+            with open("datasets/Kapodistria_scheme/Wiki_Object", 'wb') as wo:
                 pickle.dump(page, wo)
 
     region_counties = {}
@@ -95,10 +95,10 @@ def wiki_scraper():
 
     # stores dictionaries into .csv
     rc = pd.DataFrame(region_counties)
-    rc.to_csv("Kapodistria_scheme/Regions_Counties.csv", sep='\t', columns=rc.columns, index=False)
+    rc.to_csv("datasets/Kapodistria_scheme/Regions_Counties.csv", sep='\t', columns=rc.columns, index=False)
 
     cm = pd.DataFrame(counties_municipalities)
-    cm.to_csv("Kapodistria_scheme/Counties_Municipalities.csv", sep='\t', columns=cm.columns, index=False)
+    cm.to_csv("datasets/Kapodistria_scheme/Counties_Municipalities.csv", sep='\t', columns=cm.columns, index=False)
 
     return rc, cm
 
