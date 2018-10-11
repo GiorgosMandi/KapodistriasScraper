5253dc09879a7c667f34953bd553f9d42f1c2308
GiorgosMandi <giwrgosmandi@gmail.com>
2018-10-11 20:01:18 +0300

    comments and reformations

--- DatasetConstructor.py
+++ DatasetConstructor.py
@@ -1,11 +1,12 @@
 import pandas as pd
 from wiki_parser import wiki_scraper
 
-
+# dataset with the matches in yagoDateFacts
 yago_matched = pd.read_csv('datasets/yago_matched.tsv', sep='\t')
 legistlations = ('<https://el.wikipedia.org/wiki/Σχέδιο_«Καποδίστριας»>',
                  '<https://el.wikipedia.org/wiki/Πρόγραμμα_«Καλλικράτης»>')
 
+# insert legislations data in the output csv
 temporalID = ['<1>'] * 2 + ['<2>'] * 2
 subjects = [legistlations[0]] * 2 + [legistlations[1]] * 2
 predicates = ['rdf:type', 'myonto:has_label', 'rdf:type', 'myonto:has_label']
@@ -25,11 +26,14 @@ except FileNotFoundError:
 # URIs will be url to the wikipedia page
 # NOTE: URIs of municipalities doesn't reflect to the actual wiki pages
 
+# REGIONS
 regions_labels = list(rc.columns)
 regions_IDs = ["%02d" % i + '0000' for i in range(1, len(regions_labels)+1)]
-regions_URIs = ['<https://el.wikipedia.org/wiki/' + label.replace(' ', '_') + '>' for label in regions_labels]
 regions_UpperLevel = ['<Greece>'] * len(regions_labels)
+regions_URIs = ['<https://el.wikipedia.org/wiki/' + label.replace(' ', '_') + '>'
+                for label in regions_labels]
 
+# COUNTIES
 counties_labels = []
 counties_IDs = []
 counties_URIs = []
@@ -38,10 +42,11 @@ for index, r in enumerate(regions_labels):
     r_counties = list(rc[r].dropna())
     counties_labels += r_counties
     counties_IDs += ["%02d" % index + "%02d" % i + '00' for i in range(1, len(r_counties)+1)]
-    counties_URIs += ['<https://el.wikipedia.org/wiki/' + label.replace(' ', '_') + '>' for label in r_counties]
     counties_UpperLevel += [regions_URIs[index]] * len(r_counties)
+    counties_URIs += ['<https://el.wikipedia.org/wiki/' + label.replace(' ', '_') + '>'
+                      for label in r_counties]
 
-
+# MUNICIPALITIES
 municipalities_lables = []
 municipalities_IDs = []
 municipalities_URIs = []
@@ -50,12 +55,12 @@ for index, c in enumerate(counties_labels):
     c_municipalities = list(cm[c].dropna())
     municipalities_lables += c_municipalities
     municipalities_IDs += [counties_IDs[index][:-2] + "%02d" % i for i in range(1, len(c_municipalities)+1)]
+    municipalities_UpperLevel += [counties_URIs[index]] * len(c_municipalities)
     municipalities_URIs += ['<https://el.wikipedia.org/wiki/' + label.replace(' ', '_') + '>'
                             for label in c_municipalities]
-    municipalities_UpperLevel += [counties_URIs[index]] * len(c_municipalities)
 
 # all the gathered data are fused in order to create csv's columns.
-# Subjects & Objects
+# TemporalID - Subjects - Predicate - Objects
 URIs = regions_URIs + counties_URIs + municipalities_URIs
 IDs = ['<Kapodistria_' + ids + '>' for ids in regions_IDs + counties_IDs + municipalities_IDs]
 temporal_id = [ids[:-1] + '_1>' for ids in IDs]
@@ -76,12 +81,17 @@ for i in range(size):
     predicates += ['rdf:type', 'monto:hasKapodistria_ID', 'myonto:has_label',
                    'monto:has_UpperLevel', '<wasCreatedOnDate>', '<wasDestroyedOnDate>',
                    'monto:basedOn']
+
+    # in case this division was found in yagoDateFacts we connect it using sameAs predicate
+    # NOTE: I use the same URI meanig I declare an kapodistria's entity to be the same with
+    # a Kallikratis's one
     if URIs[i] in yago_matched['Wiki_Kapodistria'].values:
         index = yago_matched.index[yago_matched['Wiki_Kapodistria'] == URIs[i]][0]
         temporalID += [IDs[i][:-1] + '_2>'] * 3
         subjects += [URIs[i]] * 3
         predicates += ['<wasCreatedOnDate>', 'monto:basedOn', 'rdf:sameAs']
-        objects += ['\'2010-##-##\'^^xsd:date', legistlations[1], yago_matched['RegionUnits_yagoDate'][index]]
+        objects += ['\'2010-##-##\'^^xsd:date', legistlations[1],
+                    yago_matched['RegionUnits_yagoDate'][index]]
 
 # csv Construction
 dataset = pd.DataFrame({'TemporalID': pd.Series(temporalID),
