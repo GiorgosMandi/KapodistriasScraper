2b2b782bec5eb71c5c2bf1554bb09f1cb11b0019
GiorgosMandi <giwrgosmandi@gmail.com>
2018-10-17 12:29:54 +0300

    changes in variables' names

--- DatasetConstructor.py
+++ DatasetConstructor.py
@@ -15,14 +15,14 @@ objects = ['<LegislativeModification>', '\'Σχέδιο_«Καποδίστρια
 
 # If the files exist they are loaded, else they are generated through wiki_scraper
 try:
-    rc = pd.read_csv("datasets/Kapodistria_scheme/Regions_Counties.csv", sep='\t')
-    cm = pd.read_csv("datasets/Kapodistria_scheme/Counties_Municipalities.csv", sep='\t')
+    rc = pd.read_csv("datasets/Kapodistrias_scheme/Regions_Prefectures.csv", sep='\t')
+    cm = pd.read_csv("datasets/Kapodistrias_scheme/Prefectures_Municipalities.csv", sep='\t')
 
 except FileNotFoundError:
     rc, cm = wiki_scraper()
 
 # reads the entities from the dataframes and constructs/stores the neseccary values
-# IDs will be Kapodistria_RRCCMM where RR is region's id, CC county's id and MM municipality's id
+# IDs will be Kapodistria_RRCCMM where RR is region's id, CC prefecture's id and MM municipality's id
 # URIs will be url to the wikipedia page
 # NOTE: URIs of municipalities doesn't reflect to the actual wiki pages
 
@@ -33,41 +33,41 @@ regions_UpperLevel = ['<Greece>'] * len(regions_labels)
 regions_URIs = ['<https://el.wikipedia.org/wiki/' + label.replace(' ', '_') + '>'
                 for label in regions_labels]
 
-# COUNTIES
-counties_labels = []
-counties_IDs = []
-counties_URIs = []
-counties_UpperLevel = []
+# PREFECTURES
+prefectures_labels = []
+prefectures_IDs = []
+prefectures_URIs = []
+prefectures_UpperLevel = []
 for index, r in enumerate(regions_labels):
-    r_counties = list(rc[r].dropna())
-    counties_labels += r_counties
-    counties_IDs += ["%02d" % (index + 1) + "%02d" % i + '00' for i in range(1, len(r_counties)+1)]
-    counties_UpperLevel += [regions_URIs[index]] * len(r_counties)
-    counties_URIs += ['<https://el.wikipedia.org/wiki/' + label.replace(' ', '_') + '>'
-                      for label in r_counties]
+    r_prefectures = list(rc[r].dropna())
+    prefectures_labels += r_prefectures
+    prefectures_IDs += ["%02d" % (index + 1) + "%02d" % i + '00' for i in range(1, len(r_prefectures)+1)]
+    prefectures_UpperLevel += [regions_URIs[index]] * len(r_prefectures)
+    prefectures_URIs += ['<https://el.wikipedia.org/wiki/' + label.replace(' ', '_') + '>'
+                         for label in r_prefectures]
 
 # MUNICIPALITIES
 municipalities_lables = []
 municipalities_IDs = []
 municipalities_URIs = []
 municipalities_UpperLevel = []
-for index, c in enumerate(counties_labels):
+for index, c in enumerate(prefectures_labels):
     c_municipalities = list(cm[c].dropna())
     municipalities_lables += c_municipalities
-    municipalities_IDs += [counties_IDs[index][:-2] + "%02d" % i for i in range(1, len(c_municipalities)+1)]
-    municipalities_UpperLevel += [counties_URIs[index]] * len(c_municipalities)
+    municipalities_IDs += [prefectures_IDs[index][:-2] + "%02d" % i for i in range(1, len(c_municipalities)+1)]
+    municipalities_UpperLevel += [prefectures_URIs[index]] * len(c_municipalities)
     municipalities_URIs += ['<https://el.wikipedia.org/wiki/' + label.replace(' ', '_') + '>'
                             for label in c_municipalities]
 
 # all the gathered data are fused in order to create csv's columns.
 # TemporalID - Subjects - Predicate - Objects
-URIs = regions_URIs + counties_URIs + municipalities_URIs
-IDs = ['<Kapodistria_' + ids + '>' for ids in regions_IDs + counties_IDs + municipalities_IDs]
+URIs = regions_URIs + prefectures_URIs + municipalities_URIs
+IDs = ['<Kapodistrias_' + ids + '>' for ids in regions_IDs + prefectures_IDs + municipalities_IDs]
 temporal_id = [ids[:-1] + '_1>' for ids in IDs]
-labels = ['\'' + label + '\'' for label in regions_labels + counties_labels + municipalities_lables]
-UpperLevels = regions_UpperLevel + counties_UpperLevel + municipalities_UpperLevel
+labels = ['\'' + label + '\'' for label in regions_labels + prefectures_labels + municipalities_lables]
+UpperLevels = regions_UpperLevel + prefectures_UpperLevel + municipalities_UpperLevel
 types = ['<geoclass_first-order_administrative_division>'] * len(regions_URIs) +     \
-        ['<geoclass_second-order_administrative_division>'] * len(counties_URIs) +   \
+        ['<geoclass_second-order_administrative_division>'] * len(prefectures_URIs) +   \
         ['<geoclass_third-order_administrative_division>'] * len(municipalities_URIs)
 
 
@@ -77,7 +77,7 @@ for i in range(size):
     temporalID += [temporal_id[i]] * 7
     subjects += [URIs[i]] * 7
     objects += [types[i], IDs[i], labels[i], UpperLevels[i], '\'1997-##-##\'^^xsd:date',
-                '\'2010-##-##\'^^xsd:date', legistlations[0]]
+                '\'2011-##-##\'^^xsd:date', legistlations[0]]
     predicates += ['rdf:type', 'monto:hasKapodistria_ID', 'rdf:label',
                    'monto:has_UpperLevel', '<wasCreatedOnDate>', '<wasDestroyedOnDate>',
                    'monto:basedOn']
@@ -99,10 +99,10 @@ dataset = pd.DataFrame({'TemporalID': pd.Series(temporalID),
                         'Predicate': pd.Series(predicates),
                         'Object': pd.Series(objects)
                         })
-dataset.to_csv("datasets/Kapodistria_scheme/Kapodistria_AU.csv", sep='\t', index=False)
+dataset.to_csv("datasets/Kapodistrias_scheme/Kapodistrias_AU.csv", sep='\t', index=False)
 
 print("Regions:\t\t", len(regions_labels))
-print("Counties:\t\t", len(counties_labels))
+print("Prefectures:\t\t", len(prefectures_labels))
 print("Municipalities:\t", len(municipalities_lables))
 print("\nTotal:\t\t", len(labels))
 
--- wiki_parser.py
+++ wiki_parser.py
@@ -7,16 +7,16 @@ from bs4 import BeautifulSoup
 
 def wiki_scraper():
     try:
-        with open("datasets/Kapodistria_scheme/Wiki_Object", 'rb') as wo:
+        with open("datasets/Kapodistrias_scheme/Wiki_Object", 'rb') as wo:
             page = pickle.load(wo)
     except FileNotFoundError:
         page = requests.get("https://el.wikipedia.org/wiki/%CE%94%CE%B9%CE%BF%CE%B9%CE%BA%CE%B7%CF%84%CE%B9%CE%BA%CE%AE_%CE%B4%CE%B9%CE%B1%CE%AF%CF%81%CE%B5%CF%83%CE%B7_%CF%84%CE%B7%CF%82_%CE%95%CE%BB%CE%BB%CE%AC%CE%B4%CE%B1%CF%82_1997")
         if page.status_code == 200:
-            with open("datasets/Kapodistria_scheme/Wiki_Object", 'wb') as wo:
+            with open("datasets/Kapodistrias_scheme/Wiki_Object", 'wb') as wo:
                 pickle.dump(page, wo)
 
-    region_counties = {}
-    counties_municipalities = {}
+    region_prefectures = {}
+    prefectures_municipalities = {}
 
     # gets the table from the html page
     soup = BeautifulSoup(page.content, 'html.parser')
@@ -26,21 +26,21 @@ def wiki_scraper():
     data = sub_tables[0].find_all('td')
 
     # parses the wikipedia pages -- firstly stores the region, then stores the
-    # countie and request to the wikipedia page to get counties' municipalities.
-    # The data are stored in dictionaries where the key is the region/counties
-    # and the values are the counties/municipalities that belongs to it.
+    # prefecture and request to the wikipedia page to get prefectures' municipalities.
+    # The data are stored in dictionaries where the key is the region/prefectures
+    # and the values are the prefectures/municipalities that belongs to it.
     # NOTE: special treatment for Attica Region
     r_key = re.sub('\n', '', data[0].get_text())
-    r_counties = []
+    r_prefectures = []
     c_key = ""
-    for index, county in enumerate(data[2:]):
+    for index, prefecture in enumerate(data[2:]):
         if index % 2 == 0:
-            c_key = re.sub('\n', '', county.get_text())
-            r_counties.append(c_key)
+            c_key = re.sub('\n', '', prefecture.get_text())
+            r_prefectures.append(c_key)
 
         else:
-            print('GET ', county.get_text(), "\n")
-            url = county.find('a',  href=True)
+            print('GET ', prefecture.get_text(), "\n")
+            url = prefecture.find('a',  href=True)
             munic_page = requests.get('https://el.wikipedia.org/'+url['href'])
             sub_soup = BeautifulSoup(munic_page.content, 'html.parser')
             sub_html = list(sub_soup.children)[2]
@@ -52,28 +52,28 @@ def wiki_scraper():
                 if municipality_str[0] == ' ':
                     municipality_str = municipality_str[1:]
 
-                # NOTE: in order to skip an error in Peiraios county
+                # NOTE: in order to skip an error in Peiraios prefecture
                 if len(municipality_str.split(' ')) > 1 and \
                         (municipality_str.split(' ')[0] == "Κοινότητα" or municipality_str.split(' ')[0] == "Δήμος"):
                     municipalities.append(municipality_str)
 
-            counties_municipalities[c_key] = pd.Series(municipalities)
-    region_counties[r_key] = pd.Series(r_counties)
+            prefectures_municipalities[c_key] = pd.Series(municipalities)
+    region_prefectures[r_key] = pd.Series(r_prefectures)
 
     # for the rest of the regions
     for table in sub_tables[1:]:
         data = table.find_all('td')
         r_key = re.sub('\n', '', data[0].get_text())
-        r_counties = []
-        for index, county in enumerate(data[1:]):
+        r_prefectures = []
+        for index, prefecture in enumerate(data[1:]):
             if index % 2 == 0:
-                c_key = re.sub('\n', '', county.get_text())
-                r_counties.append(c_key)
+                c_key = re.sub('\n', '', prefecture.get_text())
+                r_prefectures.append(c_key)
 
             else:
                 # gets municipalities
-                print("GET ", county.get_text())
-                url = county.find('a',  href=True)
+                print("GET ", prefecture.get_text())
+                url = prefecture.find('a',  href=True)
                 munic_page = requests.get('https://el.wikipedia.org/'+url['href'])
                 sub_soup = BeautifulSoup(munic_page.content, 'html.parser')
                 sub_html = list(sub_soup.children)[2]
@@ -89,16 +89,16 @@ def wiki_scraper():
                             (municipality_str.split(' ')[0] == "Κοινότητα" or municipality_str.split(' ')[0] == "Δήμος"):
                         municipalities.append(municipality_str)
 
-                counties_municipalities[c_key] = pd.Series(municipalities)
+                prefectures_municipalities[c_key] = pd.Series(municipalities)
 
-        region_counties[r_key] = pd.Series(r_counties)
+        region_prefectures[r_key] = pd.Series(r_prefectures)
 
     # stores dictionaries into .csv
-    rc = pd.DataFrame(region_counties)
-    rc.to_csv("datasets/Kapodistria_scheme/Regions_Counties.csv", sep='\t', columns=rc.columns, index=False)
+    rc = pd.DataFrame(region_prefectures)
+    rc.to_csv("datasets/Kapodistrias_scheme/Regions_Prefectures.csv", sep='\t', columns=rc.columns, index=False)
 
-    cm = pd.DataFrame(counties_municipalities)
-    cm.to_csv("datasets/Kapodistria_scheme/Counties_Municipalities.csv", sep='\t', columns=cm.columns, index=False)
+    cm = pd.DataFrame(prefectures_municipalities)
+    cm.to_csv("datasets/Kapodistrias_scheme/Prefectures_Municipalities.csv", sep='\t', columns=cm.columns, index=False)
 
     return rc, cm
 
