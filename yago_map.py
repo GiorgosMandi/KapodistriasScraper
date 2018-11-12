ce2cc5b2686b09fff995059262fbe3fcbe458224
GiorgosMandi <giwrgosmandi@gmail.com>
2018-11-12 14:25:56 +0200

    feching geoname's dministrative units' labels

--- yago_map.py
+++ yago_map.py
@@ -1,39 +1,71 @@
 import pandas as pd
-
+import configparser
 
 # creates a dataset with all the administrative units that exist in
 # "datasets/yago/yagoGeonamesTypes.tsv" file
 def get_geonamesAD(config):
     filename = config['Geonames']['types_file']
-    skip_rows = 0
+    skiped_rows = 0
     step = 500000
     administrative_divisions = pd.DataFrame()
-    geoclasses = [config['Geonames']['first_order'],
-                  config['Geonames']['second_order'],
-                  config['Geonames']['third_order'],
-                  config['Geonames']['fourth_order']]
+    geoclasses = [config['Geonames']['first_order'], config['Geonames']['second_order'],
+                  config['Geonames']['third_order'], config['Geonames']['fourth_order']]
+
     # reading dataset in chunks
-    continue_flag = True
-    while continue_flag: # skip_rows < total_rows:
+    while True:
         try:
-            geonames_data = pd.read_csv(filename, header=None, skiprows=skip_rows, nrows=step, sep='\t')
+            geonames_data = pd.read_csv(filename, header=None, skiprows=skiped_rows,
+                                        nrows=step, sep='\t')
         except pd.errors.EmptyDataError:
-            continue_flag = False
+           break
+
         print(geonames_data.shape[0])
-        administrative_divisions = administrative_divisions.append\
+        administrative_divisions = administrative_divisions.append \
             (geonames_data.loc[geonames_data[3].isin(geoclasses)][[1,2,3]])
 
-        if skip_rows + step > total_rows:
-            skip_rows += total_rows - skip_rows
-        else:
-            skip_rows += step
+        skiped_rows += step
 
     print("No Rows: ",administrative_divisions.shape[0])
-    administrative_divisions.to_csv("datasets/yago/administrative_units.tsv",sep='\t', index=False)
+    administrative_divisions.to_csv(config['File_Paths']['yago_files'] +
+                                    "administrative_units.tsv",sep='\t', index=False)
+    return administrative_divisions
 
 
 
-import configparser
-config = configparser.RawConfigParser()
-config.read('config/config.ini')
-get_geonamesAD(config)
+def get_geonamesLables(config, dataset=None):
+    filename = config['Geonames']['labels_file']
+    step = 1000000
+    skiped_rows = 0
+    administrative_divisions_labels = pd.DataFrame()
+    if dataset is None:
+        dataset = pd.read_csv(config['File_Paths']['yago_files'] +
+                              "administrative_units.tsv", sep='\t', header=None)[0].values
+
+    # reading dataset in chunks
+    while True:
+        try:
+            geonames_data = pd.read_csv(filename, header=None, skiprows=skiped_rows,
+                                        nrows=step, sep='\t')
+        except pd.errors.EmptyDataError:
+            break
+
+        administrative_divisions_labels = administrative_divisions_labels.append \
+            (geonames_data.loc[geonames_data[1].isin(dataset)])[[1,2,3]]
+
+    print("No Rows: ", administrative_divisions_labels.shape[0])
+    administrative_divisions_labels.to_csv(config['File_Paths']['yago_files'] +
+                                        "administrative_units_labels.tsv", sep='\t', index=False)
+
+
+
+
+
+
+
+
+
+
+configs = configparser.RawConfigParser()
+configs.read('config/config.ini')
+#get_geonamesAD(configs)
+get_geonamesLables(configs)
