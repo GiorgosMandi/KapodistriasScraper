62cc8c40f7219cdd669206acdcd2b34056b40acc
GiorgosMandi <giwrgosmandi@gmail.com>
2018-11-21 19:12:50 +0200

    mapping date facts of administrative units

--- yago_map.py
+++ yago_map.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import rdflib as rdf
 import configparser
 
 
@@ -37,30 +38,23 @@ def get_geonamesAD(config):
 
 # finds the labels of the administrative units entities from
 # the overall yago's geonames dataset
-def get_geonamesLables(config, dataset=None):
-    filename = config['Geonames']['labels_file']
+def get_geonamesLabels(config, dataset=None):
+    filename = config['Geonames']['yago_dates']
     step = 1000000
     skiped_rows = 0
     administrative_divisions_labels = pd.DataFrame()
     if dataset is None:
-        dataset = pd.read_csv(config['File_Paths']['yago_files'] +
-                              "administrative_units.tsv", sep='\t', header=None)[0].values
+        dataset = pd.read_csv(config['File_Paths']['yago_files']+"administrative_units.tsv", sep='\t', header=None)[0].values[1:]
 
     # reads dataset in chunks
     while True:
         try:
-            geonames_data = pd.read_csv(filename, header=None, skiprows=skiped_rows,
-                                        nrows=step, sep='\t')
+            geonames_data = pd.read_csv(filename, header=None, skiprows=skiped_rows, nrows=step, sep='\t')[[1,2,3]]
         except pd.errors.EmptyDataError:
             break
-
         skiped_rows += step
-        print("Skiping ", skiped_rows)
-
-        # finds the administrative units that were stored in dataset var,
-        # and keeps their labels
-        administrative_divisions_labels = administrative_divisions_labels.append \
-            (geonames_data.loc[(geonames_data[2] == "rdfs:label") & (geonames_data[1].isin(dataset))])[[1,2,3]]
+        administrative_divisions_labels.append(geonames_data.loc[geonames_data[1].isin(dataset)])
+        print(administrative_divisions_labels)
 
     print("No Rows: ", administrative_divisions_labels.shape[0])
     administrative_divisions_labels.to_csv(config['File_Paths']['yago_files'] +
@@ -71,4 +65,4 @@ def get_geonamesLables(config, dataset=None):
 configs = configparser.RawConfigParser()
 configs.read('config/config.ini')
 #get_geonamesAD(configs)
-get_geonamesLables(configs)
+get_geonamesLabels(configs)
